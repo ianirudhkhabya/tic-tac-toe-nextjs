ea237945bad0a739422c6e9f677024d38fbfe886
anirudh <ianirudhkhabya@gmail.com>
2024-03-29 20:03:27 +0530

    Refactor Board and Square components to handle click events and display X or O values

--- components/square.tsx
+++ components/square.tsx
@@ -1,18 +1,15 @@
 "use client";
 
-import { useState } from "react";
+type SquareProps = {
+  value: string;
+  onSquareClick: () => void;
+};
 
-const Square = () => {
-  const [value, setValue] = useState<string | null>(null);
-
-  const handleClick = () => {
-    setValue("X");
-  };
-
+const Square = ({ value, onSquareClick }: SquareProps) => {
   return (
     <button
       className="h-20 w-20 border border-white border-solid items-center justify-center text-2xl font-semibold"
-      onClick={handleClick}
+      onClick={onSquareClick}
     >
       {value}
     </button>
